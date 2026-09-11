baefb20dec0c4ed57041faae72ddab6be50fd85a
Полтаранос Иван <fruty5783@gmail.com>
2026-09-11 09:56:12 +0300

    Исправлен баг

--- tmp.cpp
+++
@@ -1,9 +0,0 @@
-#include <fstream>
-
-int main() {
-    std::ofstream file("data.bin",std::ios::binary);
-    int x = 0;
-    file.write(reinterpret_cast<char*>(&x),sizeof(x));
-    file.close();
-    return 0;
-}
